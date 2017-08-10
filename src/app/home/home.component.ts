1569a7e56b0333ad7fb059c7ac24e745aa17bfb4
LeonardoDionizio <leodionizio13@gmail.com>
2017-08-10 11:08:44 -0300

    criação de serviços e promises

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from './../ofertas.service';
+import { Oferta } from './../shared/oferta.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers: [OfertasService]
 })
 export class HomeComponent implements OnInit {
+  public ofertas: Oferta[];
 
-  constructor() { }
+  constructor(private ofertasService: OfertasService) {}
 
   ngOnInit() {
+    this.ofertasService
+      .getOfertas2()
+      .then((ofertas: Oferta[]) => {
+        console.log('a funcao foi resolvida 3s');
+        this.ofertas = ofertas;
+      })
+      .catch((param: any) => {
+        console.log(param);
+      });
+    console.log(this.ofertas);
   }
-
 }
